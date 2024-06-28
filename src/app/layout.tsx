fc30a8ebbd597b34135651f8d83f88f5914f701c
ViniTanaka <vnytnk@gmail.com>
2024-06-28 00:41:02 -0300

    feat:cartcontrol

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "./components/header/header";
+import { CartControl } from "./components/header/cart-control";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Header />
       <body className={inter.className}>{children}</body>
     </html>
   );
