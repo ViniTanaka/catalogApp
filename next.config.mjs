68a2d9a850327cfa2c55c30aca9672dcbb1ff5c9
ViniTanaka <vnytnk@gmail.com>
2024-06-30 01:17:07 -0300

    configs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
     compiler:{
         styledComponents: true
-    }
+    },
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        { 
+          protocol:'https', 
+          hostname: 'cdn.dummyjson.com' 
+        }
+      ],
+    },
 };
 
 export default nextConfig;
